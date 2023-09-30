4ff7a55a57fcb60382a3caa75fd7b45a459f31bb
skufilova <alexahariwer@gmail.com>
2023-09-30 08:17:05 +0300

    hour

--- hello.pas
+++ hello.pas
@@ -1,3 +1,11 @@
 ﻿begin
-print('Привет, мир!')
+  var hour:=readinteger('Который час?');
+  case hour of
+    4..10:println('Доброе утро, мир!');
+    11..16:println('Добрый день, мир!');
+    17..22:println('Добрый вечер, мир!');
+    else println('Доброй ночи, мир!')
+  end;
 end.
+
+
