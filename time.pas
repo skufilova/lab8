e0835de238b8e3b67848b84687137d69eb4e536d
skufilova <alexahariwer@gmail.com>
2023-09-30 08:28:11 +0300

    TIME

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var N:=readinteger('Введите год:');
+  var vis:=0;
+  if (N mod 4 = 0) and ((N mod 100 <>0) or (N mod 400<>0)) then
+    println('Год високосный')
+  else println('Год невисокосный')
+end.
